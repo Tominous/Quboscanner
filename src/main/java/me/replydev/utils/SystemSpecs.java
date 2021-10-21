a13270c428276eb457e5639d158854cf9838c74f
Tominous <18189490+Tominous@users.noreply.github.com>
2021-10-21 16:47:44 +0100

    Update SystemSpecs.java

--- src/main/java/me/replydev/utils/SystemSpecs.java
+++ src/main/java/me/replydev/utils/SystemSpecs.java
@@ -2,7 +2,7 @@ package me.replydev.utils;
 
 public class SystemSpecs {
     public String getOperatingSystem() {
-        // System.out.println("Using System Property: " + os);
+         System.out.println("Using System Property: " + os);
         return System.getProperty("os.name");
     }
 }
